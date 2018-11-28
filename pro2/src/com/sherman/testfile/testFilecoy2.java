1ca9115d26a6d1df7ac649cdbfdea5b0d6b09bf5
bede1 <bede1@163.com>
2018-11-28 10:52:09 +0800

    Signed-off-by: bede1 <bede1@163.com>

---
+++ pro2/src/com/sherman/testfile/testFilecoy2.java
@@ -0,0 +1,21 @@
+package com.sherman.testfile;
+
+import java.io.IOException;
+
+/**
+ * ≤‚ ‘ ÷–¥µƒfilecopy¿‡
+ * @author Auser
+ *
+ */
+public class testFilecoy2 {
+	public static void main (String[] args) throws IOException {
+		fileCopy fc = new fileCopy();
+		fc.setInputPath("E:\\testfile\\filecopy\\filecopy.txt");
+		fc.setOutputPath("E:\\testfile\\filecopy\\filecopy2.txt");
+		fc.dofileCopy(fc.inputPath, fc.outputPath);
+		fileCopy fc2 = new fileCopy();
+		fc2.setInputPath("E:\\\\testfile\\\\filecopy\\\\filecopy3.jpg");
+		fc2.setOutputPath("E:\\\\testfile\\\\filecopy\\\\filecopy4.jpg");
+		fc2.dofileCopy(fc2.inputPath, fc2.outputPath);
+	}
+}
